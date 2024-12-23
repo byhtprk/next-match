f4b8a8d37cee9048ca954849c1d569b855befa60
Beytullah Toprak <byhtoprak@outlook.com>
2024-12-24 02:35:38 +0300

    End of section 2

---
+++ src/app/members/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+
+export default function MemberPage() {
+  return (
+    <div>
+        MemberPage<br/>
+        <Link href="/">Home</Link>
+    </div>
+    
+  )
+}
